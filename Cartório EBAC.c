a9d5bc570fa22c3820b61af013bf6604277644f9
Marcel240 <marcelleite53@yahoo.com>
2024-11-21 18:55:18 -0300

    Upload do projeto
    Projeto completo de acordo com as especificações do cliente.

---
+++ Cartório EBAC.c
@@ -0,0 +1,160 @@
+#include <stdio.h> //biblioteca de comunicaçâo com o usuario
+#include <stdlib.h> //biblioteca de alocaçâo de espaco de memoria
+#include <locale.h> //biblioteca de alocaçâo de texto por região
+#include <string.h> //biblioteca responsável por cuidar das strings
+
+
+int registro()//Função responsavel por cadastrar os usuários no sistema
+{
+	//Inicio criação de variáveis/strings
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: ");//coletando informações do usuário
+	scanf("%s", cpf);//%s refere-se a string
+	
+	strcpy(arquivo, cpf); //Responsavel por copiar os valores das string
+	
+	FILE *file; //Cria o arquivo
+	file = fopen(arquivo, "w"); //Cria o arquivo e o "w" significa escrever
+	fprintf(file,cpf); //Salva o valor da variavel
+	fclose(file); //Fecho o arquivo
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);//Fecho o arquivo
+	
+	printf("Digite o nome a ser cadastrado:");//coletando informações do usuário
+	scanf("%s", nome);//%s refere-se a string
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);//Salva o valor da variavel
+	fclose(file);//Fecho o arquivo
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);//Fecho o arquivo
+	
+	printf("Digite o sobrenome a ser cadastrado:");//coletando informações do usuário
+	scanf("%s", sobrenome);//%s refere-se a string
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,sobrenome);//Salva o valor da variavel
+	fclose(file);//Fecho o arquivo
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);//Fecho o arquivo
+	
+	printf("Digite o cargo a ser cadastrado: ");//coletando informações do usuário
+	scanf("%s",cargo);//%s refere-se a string
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);//Salva o valor da variavel
+	fclose(file);//Fecho o arquivo
+	
+	system("pause");
+}
+
+int consulta()
+{
+	setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+	
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o CPF a ser consultado: ");//coletando informações do usuário
+	scanf("%s",cpf);//%s refere-se a string
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("Não foi possivel abrir o arquivo, não localizado! \n");
+	
+	}
+
+	while(fgets(conteudo, 200, file) != NULL)	
+	{
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);//%s refere-se a string
+		printf("\n\n");
+	}
+	
+	system("pause");
+	
+	fclose(file);//Fecho o arquivo
+		
+		
+}
+
+int deletar()
+{
+	char cpf[40];
+		
+	printf("Digite o CPF usuário a ser deletado: ");//coletando informações do usuário
+	scanf("%s",cpf);//%s refere-se a string
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("O usuário não se encontra no sistema! \n");
+		system("pause");
+		
+		fclose(file);//Fecho o arquivo		
+	}	
+	
+}
+
+int main()
+{
+	int opcao=0; //Definindo as variaveis
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+		
+		system("cls");//responsavel por limpar a tela
+	
+		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+	
+		printf("### Cartório da EBAC ###\n\n"); //Inicio do menu
+		printf("Escolha a opção desejada do menu:\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+		printf("Opção:"); //Fim do menu
+
+		scanf("%d", &opcao); //Armazenando a escolha do usuário
+	
+		system("cls");//responsavel por limpar a tela
+	
+		switch(opcao)//Inicio da seleção do menu
+		{
+			case 1:
+				registro();//chamada de funções
+				break;
+			
+			case 2:
+				consulta();//chamada de funções
+				break;
+			
+			case 3:
+				deletar();//chamada de funções
+				break;
+			
+			default:
+				printf("Essa opção não está disponivel!\n");
+				system("pause");
+				break;
+		}	//Fim da seleção
+	}
+}
